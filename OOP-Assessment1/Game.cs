1844b94edd40cfa76bbaab56ce0e8ebf715da513
Unrealshadow7 <145765790+Unrealshadow7@users.noreply.github.com>
2024-03-04 16:46:43 +0000

    i have made last minute changes to my private variables to comply with the C# Guidelines

--- OOP-Assessment1/Game.cs
+++ OOP-Assessment1/Game.cs
@@ -9,22 +9,22 @@ namespace OOP_Assessment1
 {
     public class Game
     {
-        private int D1Face;private int D2Face;private int D3Face; // this creates the rolled dice value
-        private Dice D1 = new Dice(); private Dice D2 = new Dice();private Dice D3 = new Dice(); // this creates the dice 
-        private int result; //this creates the result
+        private int _D1Face;private int _D2Face;private int _D3Face; // this creates the rolled dice value
+        private Dice _D1 = new Dice(); private Dice _D2 = new Dice();private Dice _D3 = new Dice(); // this creates the dice 
+        private int _result; //this creates the result
         public void Start() // this method is just used to run the dice game
         {
-            D1Face = D1.Roll(); //this Rolls the first dice
+            _D1Face = _D1.Roll(); //this Rolls the first dice
             Console.WriteLine("Press any key to roll the dice"); Console.ReadKey(true); // this line prompts the user to roll the dice by hitting a key on their device 
-            Console.WriteLine("The value of Dice one is: " + D1Face); // this line prints the face value for the user to see what the dice rolled
-            D2Face = D2.Roll(); // this rolls the second dice 
+            Console.WriteLine("The value of Dice one is: " + _D1Face); // this line prints the face value for the user to see what the dice rolled
+            _D2Face = _D2.Roll(); // this rolls the second dice 
             Console.WriteLine("Press any key to roll the dice"); Console.ReadKey(true); // this line prompts the user to roll the dice by hitting a key on their device
-            Console.WriteLine("The value of Dice two is: " + D2Face); // this line prints the face value for the user to see what the dice rolled
-            D3Face = D3.Roll(); //this rolls the third dice
+            Console.WriteLine("The value of Dice two is: " + _D2Face); // this line prints the face value for the user to see what the dice rolled
+            _D3Face = _D3.Roll(); //this rolls the third dice
             Console.WriteLine("Press any key to roll the dice"); Console.ReadKey(true); // this line prompts the user to roll the dice by hitting a key on their device 
-            Console.WriteLine("The value of Dice three is: " + D3Face); // this line prints the face value for the user to see what the dice rolled
-            result = D1Face + D2Face + D3Face; // this line does the addtiion for all the faces rolled by the three dice 
-            Console.WriteLine("The result of the three Dice is: " + result); // this tells the user the result of the three faces added togther
+            Console.WriteLine("The value of Dice three is: " + _D3Face); // this line prints the face value for the user to see what the dice rolled
+            _result = _D1Face + _D2Face + _D3Face; // this line does the addtiion for all the faces rolled by the three dice 
+            Console.WriteLine("The result of the three Dice is: " + _result); // this tells the user the result of the three faces added togther
 
         }
         
